d3ca7d05c378016242c5103d35e97c903e375ee5
Timothy Ryon <tim@timryon.com>
2024-03-19 22:00:00 -0400

    wip

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,16 @@
+# items = (["fiddleheads", "okra", "kohlrabi"])
+items = (["kiwi", "durian", "starfruit"])
+
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        # for item in items:
+        return items[0]
+    elif len(items) == 2:
+        connect = " and "
+        return connect.join(items)
+    elif len(items) > 2:
+        (-2:-1)
+
+    
+print(oxford_comma(items))
