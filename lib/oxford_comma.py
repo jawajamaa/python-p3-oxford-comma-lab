8039095b078112e61caf157566df102a538de2eb
Timothy Ryon <tim@timryon.com>
2024-03-20 12:37:53 -0400

    works and tests all pass

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,16 +1,22 @@
 # items = (["fiddleheads", "okra", "kohlrabi"])
-items = (["kiwi", "durian", "starfruit"])
+items = (["kiwi", "durian", "starfruit","papaya", "pineapple","passion fruit", "banana"])
 
 
 def oxford_comma(items):
     if len(items) == 1:
-        # for item in items:
         return items[0]
     elif len(items) == 2:
         connect = " and "
         return connect.join(items)
     elif len(items) > 2:
-        (-2:-1)
-
+        penult = len(items)-2
+        last = len(items)+1
+        x = slice(penult,last)
+        last_two = items[x]
+        comma_list = items[:-2]
+        non_oxford = ", "
+        connect = ", and "
+        list_string = non_oxford.join(comma_list) + ", " + connect.join(last_two)
+    return list_string
     
-print(oxford_comma(items))
+# print(oxford_comma(items))
